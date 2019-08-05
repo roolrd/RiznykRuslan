6334c67a1e44cfbd1569cd4cf2a539f2816c7d60
roolrd <ruslanriznyk@gmail.com>
2019-08-05 15:21:54 +0300

    Created Person and Main

---
+++ 04_Classes_Objects_Project/Task/src/com/softserve/academy/Person.java
@@ -0,0 +1,44 @@
+package com.softserve.academy;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    LocalDate date = LocalDate.now();
+    public static int count = 0;
+    private String name;
+    private int birthYear;
+
+    public Person(){
+        count++;
+            }
+    public Person(String name,int birthYear) {
+        this.name=name;
+        this.birthYear=birthYear;
+        count++;
+    }
+
+        public void setBirthYear (int birthYear) {
+            this.birthYear=birthYear;
+        }
+        public int getBirthYear(int i){
+            return birthYear;
+        }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+        public int currAge () {
+        int calcAge = date.getYear() - birthYear;
+        return calcAge;
+   }
+
+        public void info (){
+            System.out.println("Person's name: " + name + " and he/she is " + currAge());
+        }
+}
