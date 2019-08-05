065247d5ef1646686e1e3a99c9fb6372606df54a
roolrd <ruslanriznyk@gmail.com>
2019-08-05 15:26:28 +0300

    + main once more

---
+++ 04_Classes_Objects_Project/Task/src/com/softserve/academy/Main.java
@@ -0,0 +1,21 @@
+package com.softserve.academy;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        Person person4 = new Person();
+        Person person5 = new Person();
+
+        person1.setName("Andy");
+        person1.setBirthYear(2000);
+        person1.info();
+        int numOfObj = Person.count;
+        System.out.println("We have created: " + numOfObj + " Person's objects");
+    }
+}
