cc28c32692bf120f221007c4b4664f70bf7a0436
roolrd <ruslanriznyk@gmail.com>
2019-08-06 21:06:05 +0300

    Main + Employee have created

---
+++ 04_Classes_Objects_Project/HomeWork/src/com/softserve/academy/Employee.java
@@ -0,0 +1,65 @@
+package com.softserve.academy;
+
+public class Employee {
+
+    private String name;
+    private double rate;
+    private int hours;
+    private static int totalHours;
+    private static int countObj;
+
+    Employee() {
+        countObj++;}
+
+    Employee(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+        countObj++;}
+
+    Employee(String name, double rate, int hours) {
+        this.name = name;
+        this.rate = rate;
+        this.hours = hours;
+        countObj++;}
+
+    public void setName(String name) {
+        this.name = name;}
+    public String getName() {
+        return name;}
+
+    public void setRate(double rate) {
+        this.rate = rate;}
+    public double getRate() {
+        return rate;}
+
+    public void setHours(int hours){
+        this.hours=hours;}
+    public int getHours(){
+        return hours;}
+
+    public static void setCountObj(int countObj) {
+        Employee.countObj = countObj;}
+
+    public static void setTotalHours(int totalHours) {
+        Employee.totalHours = totalHours;}
+
+    public static int getTotalHours() {
+        return totalHours;
+    }
+
+    public double sallary(int hours, double rate) {
+        double emplSallary = hours * rate;
+        return emplSallary;}
+
+    public void changeRate(double newRate) {}
+
+    public double bonuses() {
+        double bonus = rate * 0.3;
+        return bonus;}
+
+    @Override
+    public String toString() {
+        return "Employee [Name = "
+                + name + ", rate = " + rate + ", hours= " + hours + "]";}
+}
+
---
+++ 04_Classes_Objects_Project/HomeWork/src/com/softserve/academy/Main.java
@@ -0,0 +1,29 @@
+package com.softserve.academy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Employee employee1 = new Employee();
+        employee1.setName("Anatoly");
+        employee1.setRate(62.50);
+        employee1.setHours(40);
+
+        Employee employee2 = new Employee("Viktor",80.00);
+        employee2.setHours(38);
+
+        Employee employee3 = new Employee("Volodymyr",95.00, 40);
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println(employee3);
+
+        Employee [] e = {employee1, employee2, employee3};
+        int tH = 0;
+        for (Employee w : e) {
+            tH = tH + w.getHours();
+            Employee.setTotalHours(tH);}
+
+        System.out.println("Employees have worked out: " + Employee.getTotalHours() + " hours");
+    }
+}
