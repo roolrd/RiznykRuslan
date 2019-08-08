b25ddd53b6c6b673cc69e583b6900f1e3b5b6b0f
roolrd <ruslanriznyk@gmail.com>
2019-08-08 16:17:50 +0300

    static: field, block, getter

--- 06_Java-Encapsulation/HomeWork/src/com/softserve/academy/Employee.java
+++ 06_Java-Encapsulation/HomeWork/src/com/softserve/academy/Employee.java
@@ -5,12 +5,37 @@ public class Employee {
     private String name;
     private String department;
     private double salary;
-    private static int count=0;
+    private static int count;
 
-    Employee (String name, String department, double salary) {
+    static { count=0;  }
+
+    public Employee (String name, String department, double salary) {
         this.name=name;
         this.department=department;
         this.salary=salary;
-        this.count++;
+        count++;
     }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setDepartment(String department){
+        this.department=department;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setSalary(double salary){
+        this.salary=salary;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public int getCount(){
+        return count;
+    }
+
+
 }
